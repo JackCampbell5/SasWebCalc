ae6d99c7f8fa15759230f8ceab5f992164386a18
Jeff Krzywon <jkrzywon@nist.gov>
2019-11-21 16:50:59 -0500

    Create basic instrument selector tool that toggles between VSANS and traditional SANS calculators.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 ﻿// Your code here
+function changeInstrument(instrument, generalSANS, VSANS) {
+    var inst = document.getElementById(instrument)
+    var genSANS = document.getElementById(generalSANS)
+    var vSANS = document.getElementById(VSANS)
+    var selectStr = inst.options[inst.selectedIndex].value;
+    if (selectStr === "VSANS") {
+        vSANS.style.display = "block";
+        genSANS.style.display = "none";
+    } else if (selectStr === "NGB30m" || selectStr === "NGB10m" || selectStr === "NG7") {
+        genSANS.style.display = "block";
+        vSANS.style.display = "none";
+    } else {
+        genSANS.style.display = "none";
+        vSANS.style.display = "none";
+    }
+}
