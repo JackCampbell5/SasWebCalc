16983410a9159640a29e49d80d1413bc82189eb2
Jeff Krzywon <jkrzywon@nist.gov>
2019-12-13 11:28:02 -0500

    Separate javascript into multiple files; one for calculations, one for plotting, and one for the persistant state functions.

---
+++ js/persistant_state.js
@@ -0,0 +1,90 @@
+﻿/*
+ * Restore the persistant state on refresh
+ */
+function restorePersistantState() {
+    // Load instrument and sample space
+    var instrument = sessionStorage.getItem('instrument');
+    if (instrument === "") {
+        return;
+    } else if (instrument === "vsans") {
+        restoreVSANSpersistantState();
+    } else {
+        var instSelector = document.getElementById('instrumentSelector');
+        instSelector.value = instrument;
+        updateInstrument('instrumentSelector', instrument, false);
+        var sampleSpace = sessionStorage.getItem(instrument + 'SampleTable');
+        var sampleSpaceNode = document.getElementById(instrument + 'SampleTable');
+        sampleSpaceNode.value = sampleSpace;
+        // Restore wavelength values
+        var wavelengthSpread = sessionStorage.getItem(instrument + 'WavelengthSpread');
+        var wavelength = sessionStorage.getItem(instrument + 'WavelengthInput');
+        var wavelengthNode = document.getElementById(instrument + 'WavelengthInput');
+        var wavelengthSpreadNode = document.getElementById(instrument + 'WavelengthSpread').value;
+        wavelengthNode.value = wavelength;
+        wavelengthSpreadNode.value = wavelengthSpread;
+        updateWavelengthSpread(instrument, false);
+        // Restore aperture and guide configuration values
+        var customAperture = sessionStorage.getItem(instrument + 'CustomAperture');
+        var sampleAperture = sessionStorage.getItem(instrument + 'SampleAperture');
+        var guideConfig = sessionStorage.getItem(instrument + "GuideConfig");
+        var customApertureNode = document.getElementById(instrument + 'CustomAperture');
+        var sampleApertureSelectorNode = document.getElementById(instrument + 'SampleAperture');
+        var guideNode = document.getElementById(instrument + 'GuideConfig');
+        customApertureNode.value = customAperture;
+        sampleApertureSelectorNode.value = sampleAperture;
+        updateAperture(instrument, false);
+        guideNode.value = guideConfig;
+        updateGuides(instrument, guideConfig, false);
+        // Restore detector distances
+        var detectorDistance = sessionStorage.getItem(instrument + 'SDDInputBox');
+        var detectorOffset = sessionStorage.getItem(instrument + 'OffsetInputBox');
+        var detectorNode = document.getElementById(instrument + "SDDInputBox");
+        var detectorSliderNode = document.getElementById(instrument + "SDDSliderBar");
+        var offsetNode = document.getElementById(instrument + "OffsetInputBox");
+        var offsetSliderNode = document.getElementById(instrument + "OffsetSliderBar");
+        detectorNode.value = detectorDistance;
+        detectorSliderNode.value = detectorDistance;
+        offsetNode.value = detectorOffset;
+        offsetSliderNode.value = detectorOffset;
+        updateDetector(instrument, false);
+    }
+    // Run SASCALC at the end
+    SASCALC(instrument);
+}
+
+/*
+ * Placeholder for the VSANS persistant state restore function
+ * TODO: Actually write this
+ */
+function restoreVSANSpersistantState() {
+    return;
+}
+
+/*
+ * Store the persistant state for browser refreshes
+ */
+function storePersistantState(instrument) {
+    // Store instrument and sample space
+    sessionStorage.setItem('instrument', instrument);
+    var sampleSpaceNode = document.getElementById(instrument + 'SampleTable');
+    var sampleSelectStr = sampleSpaceNode.options[sampleSpaceNode.selectedIndex].value;
+    sessionStorage.setItem(instrument + 'SampleTable', sampleSelectStr);
+    // Store wavelength values
+    var wavelength = document.getElementById(instrument + 'WavelengthInput');
+    var wavelengthSpread = document.getElementById(instrument + 'WavelengthSpread').value;
+    sessionStorage.setItem(instrument + 'WavelengthSpread', wavelengthSpread);
+    sessionStorage.setItem(instrument + 'WavelengthInput', wavelength.value);
+    // Store aperture and guide configuration values
+    var customAperture = document.getElementById(instrument + 'CustomAperture');
+    var sampleApertureSelector = document.getElementById(instrument + 'SampleAperture');
+    var guideNode = document.getElementById(instrument + 'GuideConfig');
+    var guideSelectStr = guideNode.options[guideNode.selectedIndex].value;
+    sessionStorage.setItem(instrument + 'CustomAperture', customAperture.value);
+    sessionStorage.setItem(instrument + 'SampleAperture', sampleApertureSelector.value);
+    sessionStorage.setItem(instrument + "GuideConfig", guideSelectStr);
+    // Store detector distances
+    var detectorOutput = document.getElementById(instrument + "SDDInputBox");
+    var offsetOutput = document.getElementById(instrument + "OffsetInputBox");
+    sessionStorage.setItem(instrument + 'SDDInputBox', detectorOutput.value);
+    sessionStorage.setItem(instrument + 'OffsetInputBox', offsetOutput.value);
+}
---
+++ js/plotting.js
@@ -0,0 +1,53 @@
+﻿/*
+ * Update the 1D Chart with new data sets
+ */
+function update1DChart() {
+    var dataSet = {
+        x: window.qValues,
+        y: window.aveIntensity,
+        mode: 'lines+markers',
+        marker: {
+            color: window.chartColors.black,
+            size: 5,
+        },
+        line: {
+            color: window.chartColors.black,
+            size: 1,
+        },
+        name: "SASCALC"
+    };
+    var layout = {
+        title: "SASCALC 1D Plot",
+        xaxis: {
+            title: "Q (Å^-1)",
+        },
+        yaxis: {
+            title: 'Relative Intensity (Au)',
+        }
+    };
+    Plotly.newPlot('sasCalc1DChart', [dataSet], layout);
+}
+
+/*
+ * Update the 2D Chart with the current calculated 2D pattern
+ */
+function update2DChart() {
+    var dataSet = {
+        x: window.qxValues,
+        y: window.qyValues,
+        z: window.intensity2D,
+        type: 'heatmap',
+    };
+    var layout = {
+        title: "SASCALC 2D Plot",
+        xaxis: {
+            title: "Qx (Å^-1)",
+            range: [Math.min(window.qxValues), Math.max(window.qxValues)]
+        },
+        yaxis: {
+            title: "Qy (Å^-1)",
+            range: [Math.min(window.qyValues), Math.max(window.qyValues)]
+        }
+    };
+    Plotly.newPlot('sasCalc2DChart', [dataSet], layout);
+}
