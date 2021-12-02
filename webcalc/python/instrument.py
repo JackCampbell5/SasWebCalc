6b2e252d20375c908f03c5ce4170e8b89c491e18
krzywon <krzywon@yahoo.com>
2021-12-02 17:52:49 -0500

    Create Guide class to hold guide related constants and methods and Data class to hold calculation constants and methods

--- webcalc/python/instrument.py
+++ webcalc/python/instrument.py
@@ -2,7 +2,6 @@ import math
 import numpy as np
 
 from .units import Converter
-# TODO: Replace all math.unit instances with nxsunit values
 # TODO: Replace all nodes with Instrument parameters
 # TODO: Call slicer directly using known parameters
 
@@ -144,22 +143,11 @@ class Collimation:
         self.parent = parent
         self.source_aperture = Aperture(parent, params.get('source_aperture', {}))
         self.sample_aperture = Aperture(parent, params.get('sample_aperture', {}))
+        self.guides = Guide(parent, params.get('guides', {}))
         self.ssd = 0.0
         self.ssd_unit = 'cm'
         self.ssad = 0.0
         self.ssad_unit = 'cm'
-        # TODO: Put into Guide instances
-        self.guide_width = 0.0
-        self.guide_width_unit = 'cm'
-        self.transmission_per_guide = 1.0
-        self.number_of_guides = 0
-        self.lenses = False
-        self.gap_at_start = 0.0
-        self.gap_at_start_unit = 'cm'
-        self.maximum_length = 0.0
-        self.maximum_length_unit = 'cm'
-        self.length_per_guide = 0.0
-        self.length_per_guide_unit = 'cm'
         self.set_params(params)
 
     def set_params(self, params=None):
@@ -191,24 +179,12 @@ class Collimation:
     def get_ssad(self):
         return self.parent.d_converter(self.ssad, self.ssad_unit)
 
-    def get_gap_at_start(self):
-        return self.parent.d_converter(self.gap_at_start, self.gap_at_start_unit)
-
-    def get_guide_width(self):
-        return self.parent.d_converter(self.guide_width, self.guide_width_unit)
-
-    def get_length_per_guide(self):
-        return self.parent.d_converter(self.length_per_guide, self.length_per_guide_unit)
-
-    def get_maximum_length(self):
-        return self.parent.d_converter(self.maximum_length, self.maximum_length_unit)
-
     def get_sample_aperture_offset(self):
         return self.sample_aperture.get_offset()
 
     def calculate_source_to_sample_aperture_distance(self):
-        self.ssad = (self.get_maximum_length() - self.get_length_per_guide() * self.number_of_guides
-                     - self.get_sample_aperture_offset())
+        self.ssad = (self.guides.get_maximum_length() - self.guides.get_length_per_guide()
+                     * self.guides.number_of_guides - self.get_sample_aperture_offset())
 
 
 class Detector:
@@ -307,6 +283,53 @@ class Detector:
         return coefficient * math.tan((raw_value / coefficient))
 
 
+class Guide:
+    def __init__(self, parent, params):
+        # type: (Instrument, dict) -> None
+        """
+        A class for storing and manipulating detector related data.
+        Most useful for instrument with multiple detectors.
+        Args:
+            parent: The Instrument instance this Detector is a part of
+            params: A dictionary mapping <param_name>: <value>
+        """
+        self.parent = parent
+        self.guide_width = 0.0
+        self.guide_width_unit = 'cm'
+        self.transmission_per_guide = 1.0
+        self.length_per_guide = 0.0
+        self.length_per_guide_unit = 'cm'
+        self.number_of_guides = 0
+        self.lenses = False
+        self.gap_at_start = 0.0
+        self.gap_at_start_unit = 'cm'
+        self.maximum_length = 0.0
+        self.maximum_length_unit = 'cm'
+        self.set_params(params)
+
+    def set_params(self, params=None):
+        # type: (dict) -> None
+        """
+        Set class attributes based on a dictionary of values using the generic set_params function.
+        Args:
+            params: A dict mapping <param_name> -> <value> where param_name should be a known class attribute.
+        Returns: None
+        """
+        set_params(self, params)
+
+    def get_gap_at_start(self):
+        return self.parent.d_converter(self.gap_at_start, self.gap_at_start_unit)
+
+    def get_guide_width(self):
+        return self.parent.d_converter(self.guide_width, self.guide_width_unit)
+
+    def get_length_per_guide(self):
+        return self.parent.d_converter(self.length_per_guide, self.length_per_guide_unit)
+
+    def get_maximum_length(self):
+        return self.parent.d_converter(self.maximum_length, self.maximum_length_unit)
+
+
 class Wavelength:
     def __init__(self, parent, params):
         # type: (Instrument, dict) -> None
@@ -352,6 +375,96 @@ class Wavelength:
         self.wavelength_max = calculated_max if calculated_max < self.wavelength_max else self.wavelength_max
 
 
+class Data:
+    def __init__(self, parent, params):
+        # type: (Instrument, dict) -> None
+        """
+        A class for storing and manipulating collimation related data.
+        Args:
+            parent: The Instrument instance this Detector is a part of
+            params: A dictionary mapping <param_name>: <value>
+        """
+        self.parent = parent
+        self.peak_flux = np.inf
+        self.peak_wavelength = 0.0
+        self.bs_factor = 0.0
+        self.trans_1 = 0.0
+        self.trans_2 = 0.0
+        self.trans_3 = 0.0
+        self.beta = 0.0
+        self.charlie = 0.0
+        self.q_max = 6.0
+        self.q_max_horizon = 6.0
+        self.q_max_vert = 6.0
+        self.q_unit = '1/A'
+        self.q_min = 0.0
+        self.q_values = []
+        self.intensity = []
+        # Flux should be in cm^-2*s^-1
+        self.flux = 0.0
+        self.flux_size_unit = 'cm'
+        self.flux_time_unit = 's'
+        self.set_params(params)
+
+    def set_params(self, params=None):
+        # type: (dict) -> None
+        """
+        Set class attributes based on a dictionary of values using the generic set_params function.
+        Args:
+            params: A dict mapping <param_name> -> <value> where param_name should be a known class attribute.
+        Returns: None
+        """
+        set_params(self, params)
+
+    def calculate_beam_flux(self):
+        # FIXME: Flux calculation is about 7x too high
+        guide_loss = self.parent.collimation.transmission_per_guide
+        sample_aperture = self.parent.get_sample_aperture_size()
+        sdd = self.parent.get_sample_to_detector_distance()
+        wave = self.parent.get_wavelength()
+        lambda_spread = self.parent.get_wavelength_spread()
+        guides = self.parent.get_number_of_guides()
+
+        # Run calculations
+        alpha = (self.parent.get_source_aperture_size() + sample_aperture) / (2 * sdd)
+        f = (self.parent.collimation.get_gap_at_start() * alpha) / (2 * self.parent.collimation.get_guide_width())
+        trans4 = (1 - f) * (1, f)
+        trans5 = math.exp(guides * math.log(guide_loss))
+        trans6 = 1 - wave * (self.beta - (guides / 8) * (self.beta - self.charlie))
+        total_trans = self.trans_1 * self.trans_2 * self.trans_3 * trans4 * trans5 * trans6
+
+        area = math.pi * sample_aperture * sample_aperture / 4
+        d2_phi = self.peak_flux / (2 * math.pi)
+        d2_phi = d2_phi * math.exp(4 * math.log(self.peak_wavelength / wave))
+        d2_phi = d2_phi * math.exp(-1 * math.pow(self.peak_wavelength / wave, 2))
+        solid_angle = (math.pi / 4) * (sample_aperture / sdd) * (sample_aperture / sdd)
+        self.flux = area * d2_phi * lambda_spread * solid_angle * total_trans
+
+    def calculate_min_and_max_q(self, index=0):
+        sdd = self.parent.get_sample_to_detector_distance()
+        offset = self.parent.get_detector_offset()
+        wave = self.parent.get_wavelength()
+        pixel_size = self.parent.detectors[index].get_pixel_size_x()
+        det_width = pixel_size * self.parent.detectors[index].pixel_no_x
+        bs_projection = self.parent.calculate_beam_stop_projection()
+        # Calculate Q-maximum and populate the page
+        radial = math.sqrt(math.pow(0.5 * det_width, 2) + math.pow((0.5 * det_width) + offset, 2))
+        self.q_max = 4 * (math.pi / wave) * math.sin(0.5 * math.atan(radial / sdd))
+        # Calculate Q-minimum and populate the page
+        self.q_min = (math.pi / wave) * (bs_projection + pixel_size + pixel_size) / sdd
+        # Calculate Q-maximum and populate the page
+        theta = math.atan(((det_width / 2.0) + offset) / sdd)
+        self.q_max_horizon = 4 * (math.pi / wave) * math.sin(0.5 * theta)
+        # Calculate Q-maximum and populate the page
+        theta = math.atan(((det_width / 2.0) / sdd))
+        self.q_max_vert = 4 * (math.pi / wave) * math.sin(0.5 * theta)
+
+    def get_beam_flux(self):
+        self.calculate_beam_flux()
+        return (math.pow(self.parent.d_converter(self.flux, self.flux_size_unit), 2)
+                * self.parent.t_converter(self.flux, self.flux_time_unit))
+
+
 class Instrument:
     isReal = False
 
@@ -359,23 +472,8 @@ class Instrument:
         if not params:
             params = {}
         # Only store values used for calculations in Instrument class
-        self.beam_stop = {
-
-        }
-        self.calculated_values = {
-
-        }
         self.name = name
-        # TODO: Create Guide Class
-        # Current values
-        self.sample_offset = 0.0
-        self.sample_offset_unit = 'cm'
-        self.sample_aperture_offset = 0.0
-        self.sample_aperture_offset_unit = 'cm'
-        # Wavelength dictionary values
-
-        self.flux = 0.0
-        self.bs_factor = 0.0
+        # Unit converters
         self.d_converter = Converter('cm')
         self.t_converter = Converter('s')
         self.load_params(params)
@@ -383,6 +481,7 @@ class Instrument:
         self.detectors = [Detector(self, detector_params) for detector_params in params.get('detector', {})]
         self.collimation = Collimation(self, params.get('collimation', {}))
         self.wavelength = Wavelength(self, params.get('wavelength', {}))
+        self.data = Data(self, params.get('wavelength', {}))
 
     """
         Pseudo-abstract method to initialize constants associated with an instrument
@@ -394,20 +493,21 @@ class Instrument:
         # Calculate any instrument parameters
         # Keep as a separate function so Q-range entries can ignore this
         self.calculate_instrument_parameters()
+        # FIXME: What values need to be returned?
         return self.calculated_values
 
     def calculate_instrument_parameters(self):
         # Calculate the beam stop diameter
         self.calculate_beam_stop_diameter()
         # Calculate the estimated beam flux
-        self.calculate_beam_flux()
+        self.data.calculate_beam_flux()
         # Calculate the figure of merit
         self.calculate_figure_of_merit()
         # Calculate the number of attenuators
         self.calculate_attenuators()
         # Do Circular Average of an array of 1s
         for index in range(len(self.detectors) - 1):
-            self.calculate_min_and_max_q(index)
+            self.data.calculate_min_and_max_q(index)
             # TODO: This might not be needed here...
             self.calculate_q_range_slicer(index)
 
@@ -417,8 +517,8 @@ class Instrument:
         a_pixel = self.detectors[index].get_pixel_size_x()
         i_pixel_max = self.detectors[index].per_pixel_max_flux
         num_pixels = (math.pi/4) * (0.5 * (a2 + beam_diam)/a_pixel)**2
-        i_pixel = self.get_beam_flux()/num_pixels
-        atten = 1.0 if i_pixel < i_pixel_max else i_pixel_max/i_pixel
+        i_pixel = self.get_beam_flux() / num_pixels
+        atten = 1.0 if i_pixel < i_pixel_max else i_pixel_max / i_pixel
         self.wavelength.attenuation_factor = atten if atten == 1.0 else atten.toNumeric()
 
     def calculate_attenuators(self):
@@ -442,7 +542,7 @@ class Instrument:
         sdd = self.get_sample_aperture_to_detector_distance(index)
         wavelength = self.get_wavelength()
         wavelength_spread = self.get_wavelength_spread()
-        if self.collimation.lenses:
+        if self.collimation.guides.lenses:
             # If LENS configuration, the beam size is the source aperture size
             # FIXME: This is showing -58 cm... Why?!?!
             self.beam_stops[index].value = self.get_source_aperture_size()
@@ -453,7 +553,7 @@ class Instrument:
         bv4 = bv3 * wavelength_spread
         bv = beam_width + 0.0000125 * bv4
         # Larger of the width*safetyFactor and height
-        bm_bs = self.bs_factor * beam_width
+        bm_bs = self.data.bs_factor * beam_width
         bm = bm_bs if bm_bs > bv else bv
         if direction == 'vertical':
             beam_diam = bv
@@ -485,68 +585,9 @@ class Instrument:
         l_beam_stop = 20.1 + 1.61 * self.get_beam_stop_diameter()  # distance in cm from beam stop to anode plane
         return bs_diam + (bs_diam + sample_aperture) * l_beam_stop/(l2 - l_beam_stop)  # Return in cm
 
-    def calculate_beam_flux(self):
-        # FIXME: Flux calculation is about 7x too high
-        # Get constants
-        # TODO: Get values from correct locations
-        peak_lambda = self.flux.peakWavelength
-        peak_flux = self.flux.peakFlux
-        guide_gap = self.collimation.get_gap_at_start()
-        guide_loss = self.collimation.transmission_per_guide
-        guide_width = self.collimation.get_guide_width()
-        trans1 = self.flux.trans1
-        trans2 = self.flux.trans2
-        trans3 = self.flux.trans3
-        b = self.flux.b
-        c = self.flux.c
-        source_aperture = self.get_source_aperture_size()
-        sample_aperture = self.get_sample_aperture_size()
-        sdd = self.get_sample_to_detector_distance()
-        wave = self.get_wavelength()
-        lambda_spread = self.get_wavelength_spread()
-        guides = self.get_number_of_guides()
-
-        # Run calculations
-        alpha = (source_aperture + sample_aperture) / (2 * sdd)
-        f = (guide_gap * alpha) / (2 * guide_width)
-        trans4 = (1 - f) * (1, f)
-        trans5 = math.exp(guides * math.log(guide_loss))
-        trans6 = 1 - wave * (b - (guides / 8) * (b - c))
-        total_trans = trans1, trans2, trans3 * trans4 * trans5 * trans6
-
-        area = math.pi * sample_aperture * sample_aperture / 4
-        d2_phi = peak_flux / (2 * math.pi)
-        d2_phi = d2_phi * math.exp(4 * math.log(peak_lambda / wave))
-        d2_phi = d2_phi * math.exp(-1 * math.pow(peak_lambda / wave, 2))
-        solid_angle = (math.pi / 4) * (sample_aperture / sdd) * (sample_aperture / sdd)
-        beam_flux = area * d2_phi * lambda_spread * solid_angle * total_trans
-        # TODO: Store beam_flux in appropriate location
-
     def calculate_figure_of_merit(self):
         figure_of_merit = math.pow(self.get_wavelength(), 2) * self.get_beam_flux()
         return figure_of_merit.toNumeric()
-
-    def calculate_min_and_max_q(self, index=0):
-        sdd = self.get_sample_to_detector_distance()
-        offset = self.get_detector_offset()
-        wave = self.get_wavelength()
-        # TODO: Get actual values from classes, as needed
-        pixel_size = self.detectors[index].get_pixel_size_x()
-        det_width = pixel_size * self.detectors[index].pixel_no_x
-        bs_projection = self.calculate_beam_stop_projection()
-        # Calculate Q-maximum and populate the page
-        radial = math.sqrt(math.pow(0.5 * det_width, 2) + math.pow((0.5 * det_width) + offset, 2))
-        # TODO: Store q_max and q_min in appropriate places
-        q_max = 4 * (math.pi / wave) * math.sin(0.5 * math.atan(radial / sdd))
-        # Calculate Q-minimum and populate the page
-        q_min = (math.pi / wave) * (bs_projection + pixel_size + pixel_size) / sdd
-        # Calculate Q-maximum and populate the page
-        theta = math.atan(((det_width / 2.0) + offset) / sdd)
-        q_max_horizon = 4 * (math.pi / wave) * math.sin(0.5 * theta)
-        # Calculate Q-maximum and populate the page
-        theta = math.atan(((det_width / 2.0) / sdd))
-        q_max_vert = 4 * (math.pi / wave) * math.sin(0.5 * theta)
-        # TODO: Store q_max_vert and q_max_horizon in appropriate places
 
     # Various class updaters
     def update_wavelength(self, run_sas_calc=True):
@@ -558,15 +599,15 @@ class Instrument:
     # Use these to be sure units are correct
     def get_attenuation_factor(self):
         # The attenuation factor value calculated based on the number of attenuators
-        return self.attenuationFactorNode.value
+        return self.get_attenuation_factor()
 
     def get_attenuators(self):
         # Number of attenuators in the beam
-        return self.attenuatorNode.value
+        return self.calculate_attenuators()
 
     def get_beam_flux(self):
         # Beam flux in cm^-2-s^-1
-        return self.beamfluxNode.value
+        return self.data.get_beam_flux()
 
     def get_beam_diameter(self, index=0):
         # Beam diameter in centimeters
@@ -579,7 +620,7 @@ class Instrument:
 
     def get_number_of_guides(self):
         # Number of neutron guides in the beam
-        guides = self.collimation.number_of_guides
+        guides = self.collimation.guides.number_of_guides
         if guides == "LENS":
             guides = 0
         else:
@@ -602,21 +643,16 @@ class Instrument:
             return 0.0
         return detector.get_sadd()
 
-    def get_sample_offset(self):
-        return self.d_converter(self.sample_offset, self.sample_offset_unit)
-
     def get_sample_aperture_offset(self):
-        return self.d_converter(self.sample_aperture_offset, self.sample_aperture_offset_unit)
+        return self.collimation.get_sample_aperture_offset()
 
     def get_sample_to_detector_distance(self, index=0):
         # SDD in centimeters
-        table_offset = self.get_sample_offset()
         try:
             detector = self.detectors[index]
         except IndexError:
             return 0.0
-        sdd = detector.get_ssd()
-        return sdd + table_offset
+        return detector.get_ssd()
 
     def get_detector_offset(self, index=0):
         # Detector offset in centimeters
