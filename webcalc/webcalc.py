5aaa3016794c3aed98212ffec0b643ade9e50207
krzywon <krzywon@yahoo.com>
2021-12-02 12:47:17 -0500

    Add ability to call instrument class and change variable so it doesn't shadow outer scope

--- webcalc/webcalc.py
+++ webcalc/webcalc.py
@@ -4,7 +4,8 @@ import numpy as np
 from flask import Flask, render_template, request
 
 from python.link_to_sasmodels import get_model_list, get_params
-from python.link_to_sasmodels import calculate_model as calculate
+from python.link_to_sasmodels import calculate_model as calculate_m
+from python.instrument import calculate_instrument as calculate_i
 from python.helpers import decode_json
 
 
@@ -31,7 +32,20 @@ def create_app():
         else:
             q = [np.array(data[0][2])]
         params = {param_names[i]: param_values[i] for i in range(len(param_values))}
-        return calculate(model_name, q, params)
+        return calculate_m(model_name, q, params)
+
+    @app.route('/calculateresolution/<instrument_name>', methods=['POST'])
+    def calculate_instrument(instrument_name):
+        data = decode_json(request.data)
+        param_names = data[0][0]
+        param_values = data[0][1]
+        if len(data[0]) > 3:
+            q = [np.array([np.tile(x, len(data[0][3])) for x in data[0][2]]).flatten(),
+                 np.array(np.tile(data[0][3], (1, len(data[0][2])))).flatten()]
+        else:
+            q = [np.array(data[0][2])]
+        params = {param_names[i]: param_values[i] for i in range(len(param_values))}
+        return calculate_i(instrument_name, params)
 
     @app.route('/getparams/<model_name>', methods=['GET'])
     def get_model_params(model_name):
@@ -42,5 +56,5 @@ def create_app():
 
 if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8002
-    app = create_app()
-    app.run(port=port, debug=True)
+    sas_calc = create_app()
+    sas_calc.run(port=port, debug=True)
