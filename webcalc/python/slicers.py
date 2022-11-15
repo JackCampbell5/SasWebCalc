4847a7166a6e97c3f04811f308b52fcb7f8ba45c
jbc7 <jack.campbell@nist.gov>
2022-11-15 09:12:10 -0500

    Finished calculate method in slicer class and created many helper methods

--- webcalc/python/slicers.py
+++ webcalc/python/slicers.py
@@ -32,6 +32,15 @@ def set_params(instance, params):
             print(f"The parameter {key} is not a known {instance} attribute. Unable to set it to {value}.")
 
 
+def create_plot():
+    return []
+
+
+# Question are x_val and y-val needed in this method
+def include_pixel(x_val, y_val, mask):
+    return mask == 0
+
+
 class Slicer:
 
     def __init__(self, params):
@@ -49,6 +58,11 @@ class Slicer:
         # Q values
         self.qx_values = []
         self.qy_values = []
+        self.q_values = [0]
+        self.ave_intensity = [0]
+        self.d_sq = [0]
+        self.n_cells = [0]
+        self.sigma_ave = [0]
         # Max and Min Q values
         self.max_qx = 0.0
         self.max_qy = 0.0
@@ -97,10 +111,10 @@ class Slicer:
         # Sets the max and min q values and all the phi values
 
         # Min and max Q value
-        self.max_qx = 0.3 if max(self.qx_values) == 0.0 else max(self.qx_values)
-        self.max_qy = 0.3 if max(self.qy_values) == 0.0 else max(self.qy_values)
-        self.min_qx = 0.0 if min(self.qx_values) == 0.0 else min(self.qx_values)
-        self.min_qy = 0.0 if min(self.qy_values) == 0.0 else min(self.qy_values)
+        self.max_qx = 0.3 if len(self.qx_values) == 0 else max(self.qx_values)
+        self.max_qy = 0.3 if len(self.qy_values) == 0 else max(self.qy_values)
+        self.min_qx = 0.0 if len(self.qx_values) == 0 else min(self.qx_values)
+        self.min_qy = 0.0 if len(self.qy_values) == 0 else min(self.qy_values)
 
         # Calculated parameters
         self.phi_upper = self.phi * self.d_phi
@@ -111,6 +125,87 @@ class Slicer:
         self.phi_to_ul_corner = math.atan(self.max_qy / self.min_qx) + math.pi
         self.phi_to_ll_corner = math.atan(self.min_qy / self.min_qx) + math.pi
         self.phi_to_lr_corner = math.atan(self.min_qy / self.max_qx) + 2 * math.pi
+
+    def calculate(self):
+        nq = 0
+        large_number = 1.0
+        radius_center = 100
+        data = self.intencity_2D
+        data_i = []
+        mask_i = []
+        num_dimensions = 1
+        center = 1
+        for i in range(self.qx_values):
+            qx_val = self.qx_values[i]
+            x_distence = self.calculate_distence_from_beam_center(i, "x")
+            mask_i = self.mask[i]
+            data_i = data[i]
+            for j in range(self.qy_values):
+                qy_val = self.qy_values[j]
+                if include_pixel(qx_val, qy_val, mask_i[j]):
+                    y_distance = calculate_distance_from_beam_center(j, "y")
+                    data_pixel = data_i[j]
+                    total_distence = math.sqrt(x_distence * x_distence + y_distance * y_distance)
+                    # breaks pixels up into a 3*3 grid close to beam center
+                    if total_distence > radius_center:
+                        num_dimensions = 1
+                        center = 1
+                    else:
+                        num_dimensions = 3
+                        center = 2
+                    num_d_squared = num_dimensions * num_dimensions
+                    # Loop over sliced pixels
+                    for k in range(1, num_dimensions + 1):
+                        corrected_dx = x_distence + (k - center) * self.pixel_size / num_dimensions
+                        for l in range(1, num_dimensions + 1):
+                            corrected_dy = y_distance + (l - center) * self.pixel_size / num_dimensions
+                            i_radius = self.get_i_radius(corrected_dx, corrected_dy)
+                            nq = i_radius if i_radius > nq else nq
+
+                            # before reformated  self.ave_intensity[i_radius] = data_pixel/num_d_squared if self.ave_intensity[i_radius] is None else self.ave_intensity[i_radius]+data_pixel/num_d_squared
+                            #                             self.d_sq[i_radius] = data_pixel*data_pixel/num_d_squared if self.d_sq[i_radius] is None else self.d_sq[i_radius]+data_pixel*data_pixel/num_d_squared
+                            #                             self.n_cells[i_radius] = 1/num_d_squared if self.n_cells[i_radius] is None else self.n_cells[i_radius]+1 /num_d_squared
+                            self.ave_intensity[i_radius] = data_pixel / num_d_squared if self.ave_intensity[
+                                                                                             i_radius] is None else \
+                            self.ave_intensity[i_radius] + data_pixel / num_d_squared
+                            self.d_sq[i_radius] = data_pixel * data_pixel / num_d_squared if self.d_sq[
+                                                                                                 i_radius] is None else \
+                            self.d_sq[i_radius] + data_pixel * data_pixel / num_d_squared
+                            self.n_cells[i_radius] = 1 / num_d_squared if self.n_cells[i_radius] is None else \
+                            self.n_cells[i_radius] + 1 / num_d_squared
+        for i in range(nq):
+            self.calculate_q(i)
+            if self.n_cells[i] <= 1:
+                self.ave_intensity[i] = 0 if self.n_cells[i] == 0 or math.isnan(self.n_cells[i]) else \
+                self.ave_intensity[i] / self.n_cells[i]
+                self.sigma_ave[i] = large_number
+            else:
+                self.ave_intensity[i] = self.ave_intensity[i] if math.isnan(self.n_cell[i]) else self.ave_intensity[i] / \
+                                                                                                 self.n_cells[i]
+                ave_sq = self.ave_intensity[i] * self.ave_intensity[i]
+                ave_isq = self.d_sq[i] if math.isnan(self.n_cells[i]) else self.d_sq[i] / self.n_cells[i]
+                diff = ave_isq - ave_sq
+                self.sigma_ave[i] = large_number if diff < 0 else math.sqrt(diff / self.n_cells[i] - 1)
+            if self.q_values[i] > 0.0:
+                self.calculate_resolution(i)
+
+    def calculate_q(self, i):
+        radius = (2 * i) * self.pixel_size / 2
+        theta = math.atan(radius / self.SDD) / 2
+        self.q_values[i] = (4 * math.pi / self.lambda_val) * math.sin(theta)
+
+    def get_i_radius(self, x_val, y_val):
+        return math.floor(math.sqrt(x_val * x_val + y_val * y_val) / self.pixel_size) + 1
+
+    def calculate_resolution(self, i):
+        pass
+
+    def calculate_distance_from_beam_center(self, pixel_value, x_or_y):
+        if x_or_y.lower() == "x":
+            pixel_center = self.x_center
+        else:
+            pixel_center = self.y_center
+        return self.coeff * math.tan(pixel_value - pixel_center) * self.pixel_size / self.coeff
 
     # Calculate Q Range Slicer and its helper methods
     def calculate_q_range_slicer(self):
