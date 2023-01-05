b38af95fdda184a0b801d7b9c307afe15caf4afd
krzywon <krzywon@yahoo.com>
2023-01-05 16:32:10 -0500

    Create NG7 instrument class and enable in drop-down

--- webcalc/static/App.js
+++ webcalc/static/App.js
@@ -65,7 +65,7 @@ const template = `
 const instruments = {
   "q_range": "User-Defined Q-Range and Resolutions",
   //"ngb30": "NGB 30m SANS",
-  //"ng7": "NG7 SANS",
+  "ng7": "NG7 SANS",
   //"ngb10": "NGB 10m SANS",
   //"vsans": "VSANS",
 };
--- webcalc/static/instruments/NG7SANS.js
+++ webcalc/static/instruments/NG7SANS.js
@@ -1,17 +1,39 @@
 const template = `
 <div class="instrument_params">
   <h3>{{title}}</h3>
+  <div id="ng7_inputs">
+    <div class="param" v-for="(param, key) in instrument_params" :key="key">
+        <label :for="key">{{param.name}}:&nbsp;</label>
+        <select v-if="param.type == 'select'" v-model.string="param.default" :id="key" @change="onChangeValue">
+            <option v-for="option in param.options" :key="option" :value="option">{{option}}</option>
+        </select>
+        <input v-else type="number" v-model.number="param.default" :id="key" class="fixed-width-input" 
+            :min="(param.lower_limit == '-inf') ? null : param.lower_limit"
+            :max="(param.upper_limit == 'inf') ? null : param.upper_limit"
+            @input="onChangeValue"/>
+        <span v-if="param.unit != ''">&nbsp;{{param.unit}}</span>
+    </div>
+  </div>
 </div>
 `
 
 export default {
   props: {
-    title: String
+    title: String,
+    instrument_params: {type: Object, default: {}},
   },
-  data: () => ({
-    params: {
+  methods: {
+   onChangeValue (event) {
+       this.$emit('valueChange', this.instrument_params);
+   }
+  },
+  data() {
+    return {
+      instrument_params: {
+
+      }
 
     }
-  }),
+  },
   template: template
 }
