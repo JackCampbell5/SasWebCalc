9a2d8a4b229d34a45837ae916e2d5a482c0e37c5
krzywon <krzywon@yahoo.com>
2023-01-09 16:55:09 -0500

    Create nested loop that separates input types by category

--- webcalc/static/instruments/NG7SANS.js
+++ webcalc/static/instruments/NG7SANS.js
@@ -1,18 +1,23 @@
 const template = `
 <div class="instrument_params">
-  <h3>{{title}}</h3>
+  <h2>{{title}} Instrumental Parameters</h2>
   <div id="ng7_inputs">
-    <div class="param" v-for="(param, key) in instrument_params" :key="key">
-        <label :for="key">{{param.name}}:&nbsp;</label>
-        <select v-if="param.type == 'select'" v-model.string="param.default" :id="key" @change="onChangeValue">
-            <option v-for="option in param.options" :key="option" :value="option">{{option}}</option>
-        </select>
-        <input v-else type="number" v-model.number="param.default" :id="key" class="fixed-width-input" 
-            :min="(param.lower_limit == '-inf') ? null : param.lower_limit"
-            :max="(param.upper_limit == 'inf') ? null : param.upper_limit"
-            @input="onChangeValue"/>
-        <span v-if="param.unit != ''">&nbsp;{{param.unit}}</span>
-    </div>
+    <div v-for="(category, cat_key) in categories" :id="cat_key" :key="cat_key" class="instrument-section" >
+      <h3>{{category.display_name}}:</h3>
+      <div class="parameter" v-for="(param, key) in instrument_params" :key="key">
+        <span v-if="param.category == cat_key">
+          <label :for="key">{{param.name}}:&nbsp;</label>
+          <select v-if="param.type == 'select'" v-model.string="param.default" :id="key" @change="onChangeValue">
+              <option v-for="option in param.options" :key="option" :value="option">{{option}}</option>
+          </select>
+          <input v-else type="number" v-model.number="param.default" :id="key" class="fixed-width-input" 
+              :min="(param.lower_limit == '-inf') ? null : param.lower_limit"
+              :max="(param.upper_limit == 'inf') ? null : param.upper_limit"
+              @input="onChangeValue"/>
+          <span v-if="param.unit != ''">&nbsp;{{param.unit}}</span>
+         </span>
+      </div>
+     </div>
   </div>
 </div>
 `
@@ -21,6 +26,7 @@ export default {
   props: {
     title: String,
     instrument_params: {type: Object, default: {}},
+    categories: {type: Object, default: {}},
   },
   methods: {
    onChangeValue (event) {
@@ -29,8 +35,31 @@ export default {
   },
   data() {
     return {
+      title: "NG7 SANS",
+      categories: {
+        "ng7Sample": {
+          display_name: 'Sample Space'
+        },
+      },
       instrument_params: {
-
+        'ng7_sample_table': {
+          name: 'Sample Table',
+          default: "Chamber",
+          type: "select",
+          options: [
+            'Chamber',
+            'Huber'
+          ],
+          unit: '',
+          category: 'ng7Sample',
+        },
+        'ng7_sdd': {
+          name: 'Sample-To-Detector-Distance',
+          default: 100,
+          type: "number",
+          unit: 'cm',
+          category: 'ng7Detector',
+        },
       }
 
     }
