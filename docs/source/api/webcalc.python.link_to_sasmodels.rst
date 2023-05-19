eb1398fd2d0a7895042fc404f27c4e37d2a34447
Jack Campbell <jack.campbell@nist.gov>
2023-05-19 09:37:53 -0400

    Finished documentation of link_to_sasmodels.py file

--- docs/source/api/webcalc.python.link_to_sasmodels.rst
+++ docs/source/api/webcalc.python.link_to_sasmodels.rst
@@ -1,6 +1,15 @@
 webcalc\.python\.link_to_sasmodels
 ==================================
 
+Imports
+-------
++ json
++ sys
++ numpy as np
++ list_models and load_model from sasmodels.core
++ call_kernel from sasmodels.direct_model
++ encode_json from .helpers
+
 .. automodule:: webcalc.python.link_to_sasmodels
     :members:
     :undoc-members:
--- webcalc/python/link_to_sasmodels.py
+++ webcalc/python/link_to_sasmodels.py
@@ -10,16 +10,36 @@ from .helpers import encode_json
 
 
 def get_model_list(category=None):
+    """Gets the model list from sasmodels
+
+    :param String category: A string value to be passed to the list models parameter in sasmodels
+    :return: A list of the model list
+    :rtype: List
+    """
+    print(encode_json(list_models(category)))
     return encode_json(list_models(category))
 
 
 def get_model(model_string):
+    """Loads model params for the specified model
+
+    :param str model_string:
+    :return: A PyModel object that contains
+    :rtype: PyModel
+    """
+    print(load_model(model_string))
     if not model_string:
         return None
     return load_model(model_string)
 
 
 def encode_params(params):
+    """Encodes the parameters gotten from sasmodels as a dictionary
+
+    :param PyModel params: The pymodel object that contains the params
+    :return: An encoded Json that contains the params encoded correctly
+    :rtype: JSON or DllModel object
+    """
     return_params = {}
     param_dict = {}
     for param in params:
@@ -32,10 +52,23 @@ def encode_params(params):
 
 
 def get_all_params(model_string):
+    """Gets all the params by passing True to the get params method
+
+    :param str model_string: THe string name of the model
+    :return: The list of params
+    :rtype: list
+    """
     return get_params(model_string, True)
 
 
 def get_params(model_string, all=False):
+    """Gets most of the params by passing False to the get params method
+
+    :param str model_string: The string name of the model
+    :param all: whether it is all the params or not
+    :return: The list of the params encoded
+    :rtype: list
+    """
     model = get_model(model_string) if model_string else None
     if all and model:
         model = get_model(model_string)
@@ -50,6 +83,14 @@ def get_params(model_string, all=False):
 
 
 def calculate_model(model_string, q, params):
+    """ Takes the model and runs a sequence of code to calculate it
+
+    :param str model_string: The string name of the model
+    :param q:
+    :param list params: THe list of params probably passed from a previous method
+    :return: A list of calculated data from the model
+    :rtype: list
+    """
     kernel_model = get_model(model_string)
     kernel = kernel_model.make_kernel(q)
     Iq = call_kernel(kernel, params)
