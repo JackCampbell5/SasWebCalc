5444814f66d3cd4614dc8fa1ebc305b26acaea75
krzywon <krzywon@yahoo.com>
2023-06-15 10:15:11 -0400

    Include gunicorn in setup to allow docker to run

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='SasWebCalc',
     version='0.1.0',
-    url='https://github.com/mypackage.git',
+    url='https://gitlab.nist.gov/gitlab/jkrzywon/saswebcalc.git',
     author='Jeffery Krzywon and Jack Campbell',
     author_email='jeffery.krzywon@nist.gov',
     description='A web-based small-angle scattering (SAS) tool for calculating theoretical I vs. Q and 2D scattering patterns based off an instrumental configuration and SAS model.',
     packages=find_packages(),
-    install_requires=['numpy', 'flask', 'SasModels'],
+    install_requires=['numpy', 'flask', 'sasmodels', 'gunicorn'],
 )
