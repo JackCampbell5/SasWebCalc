1bf8db4badf118bfb5cde5f3ad88689ef1dfaf8c
jbc7 <jack.campbell@NIST.gov>
2023-06-19 11:25:30 -0400

    Added new paths for structures and updated the model name to include structure factor if necessary

--- webcalc/webcalc.py
+++ webcalc/webcalc.py
@@ -9,13 +9,13 @@ from flask import Flask, render_template, request, send_file
 
 # import specific methods from python files
 try:
-    from python.link_to_sasmodels import get_model_list, get_params
+    from python.link_to_sasmodels import get_model_list, get_params, get_structure_list
     from python.link_to_sasmodels import calculate_model as calculate_m
     from python.instrument import calculate_instrument as calculate_i
     from python.helpers import decode_json, encode_json
 except ModuleNotFoundError:
     # Runs the imports from webcalc only when auto-doc runs to remove errors
-    from webcalc.python.link_to_sasmodels import get_model_list, get_params
+    from webcalc.python.link_to_sasmodels import get_model_list, get_params, get_structure_list
     from webcalc.python.link_to_sasmodels import calculate_model as calculate_m
     from webcalc.python.instrument import calculate_instrument as calculate_i
     from webcalc.python.helpers import decode_json, encode_json
@@ -61,6 +61,10 @@ def create_app():
     def get_all_models():
         return get_model_list()
 
+    @app.route('/get/structures/', methods=['GET'])
+    def get_all_structures():
+        return get_structure_list()
+
     @app.route('/get/params/<model_name>', methods=['GET'])
     @app.route('/get/params/model/<model_name>', methods=['GET'])
     def get_model_params(model_name):
@@ -82,7 +86,11 @@ def create_app():
         instrument_params = json_like.get('instrument_params', {})
 
         # Gets the model and model params out of the dict
+        structure_factor = json_like.get('structure_factor','None')
+        is_structure_factor = True if structure_factor != 'None' else False
         model = json_like.get('model', '')
+        if is_structure_factor:
+            model = model+"@"+structure_factor
         model_params = json_like.get('model_params', {})
         model_params = {key: value.get('default', 0.0) for key, value in model_params.items()}
 
