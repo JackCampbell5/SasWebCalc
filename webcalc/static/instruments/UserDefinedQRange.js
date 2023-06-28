d4e1e16c5b1b840bb93239a891a9db676f09a341
Jack Campbell <jack.campbell@nist.gov>
2023-06-28 14:49:10 -0400

    Updated UserDefinedQRange.js so the user input boxes would step through values correctly

--- webcalc/static/instruments/UserDefinedQRange.js
+++ webcalc/static/instruments/UserDefinedQRange.js
@@ -10,6 +10,7 @@ const template = `
         <input v-else type="number" v-model.number="param.default" :id="key" class="fixed-width-input" 
             :min="(param.lower_limit == '-inf') ? null : param.lower_limit"
             :max="(param.upper_limit == 'inf') ? null : param.upper_limit"
+            :step="(param.step!== undefined) ? param.step : null"
             @input="onChangeValue"/>
         <span v-if="param.unit != ''">&nbsp;{{param.unit}}</span>
     </div>
@@ -33,56 +34,61 @@ export default {
           name: 'Q Min Vertical',
           default: -0.3,
           type: "number",
-          min: -2.0,
-          max: 2.0,
+          lower_limit: -2.0,
+          upper_limit: 2.0,
           unit: 'Å',
+          step: 0.1,
         },
         'q_max_vertical': {
           name: 'Q Max Vertical',
           default: 0.3,
           type: "number",
-          min: -2.0,
-          max: 2.0,
+          lower_limit: -2.0,
+          upper_limit: 2.0,
           unit: 'Å',
+          step: 0.1,
         },
         'q_min_horizontal': {
           name: 'Q Min Horizontal',
           default: -0.3,
           type: "number",
-          min: -2.0,
-          max: 2.0,
+          lower_limit: -2.0,
+          upper_limit: 2.0,
           unit: 'Å',
+          step: 0.1,
         },
         'q_max_horizontal': {
           name: 'Q Max Horizontal',
           default: 0.3,
           type: "number",
-          min: -2.0,
-          max: 2.0,
+          lower_limit: -2.0,
+          upper_limit: 2.0,
           unit: 'Å',
+          step: 0.1,
         },
         'q_min': {
           name: 'Q Minimum',
           default: 0.01,
           type: "number",
-          min: -2.0,
-          max: 2.0,
+          lower_limit: -2.0,
+          upper_limit: 2.0,
           unit: 'Å',
+          step: 0.01,
         },
         'dq': {
           name: 'Q Resolution',
           default: 10.0,
           type: "number",
-          min: 0.0,
-          max: 'inf',
+          lower_limit: 0.0,
+          upper_limit: 'inf',
           unit: '%',
         },
         'points': {
           name: 'Number of 1D points',
           default: 50,
           type: "number",
-          min: 0.0,
-          max: 1000,
+          lower_limit: 0.0,
+          upper_limit: 1000,
           unit: '',
         },
         'point_spacing': {
